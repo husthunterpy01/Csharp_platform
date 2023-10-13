fc90948a25835a7ef2a981871b365e33d4a0f6d6
husthunterpy01 <dangminhhust193231@gmail.com>
2023-10-13 20:36:03 +0700

    Winform platform demo

---
+++ LabelGUI/LabelGUI/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace LabelGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Bonjour"; // Show dialog 
+        }
+
+        void LoadLabel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Label lb = new Label();
+                lb.Text = "Oh"; // Set the text for each label
+                lb.BackColor = Color.Aqua;
+                this.Controls.Add(lb); // Add the label to the Form1's controls
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "Free Education"; 
+            Form2 f = new Form2(); 
+            /*f.Show(); */ // Pop up new form, new forms can still pop up after continuing pressing the label
+            f.ShowDialog(); //Pop up new form, only 1 new form appears and no other boxes will apear, applied for passsword or notification
+            f.lb2.Text = "Share to be better";
+        }
+    }
+}
---
+++ LabelGUI/LabelGUI/Form2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LabelGUI
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(79, 170);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(44, 16);
+            this.lb2.TabIndex = 0;
+            this.lb2.Text = "label2";
+            this.lb2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb2);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lb2;
+    }
+}
