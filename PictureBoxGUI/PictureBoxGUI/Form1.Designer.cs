ea570c0b8f04e2fe91c69ff62e68eaf909958a1c
husthunterpy01 <dangminhhust193231@gmail.com>
2023-10-17 13:39:26 +0700

    Picture box

---
+++ PictureBoxGUI/PictureBoxGUI/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PictureBoxGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbImage = new System.Windows.Forms.PictureBox();
+            this.cbImage = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbImage
+            // 
+            this.pcbImage.Location = new System.Drawing.Point(218, 40);
+            this.pcbImage.Name = "pcbImage";
+            this.pcbImage.Size = new System.Drawing.Size(260, 261);
+            this.pcbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pcbImage.TabIndex = 0;
+            this.pcbImage.TabStop = false;
+            this.pcbImage.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // cbImage
+            // 
+            this.cbImage.FormattingEnabled = true;
+            this.cbImage.Location = new System.Drawing.Point(31, 40);
+            this.cbImage.Name = "cbImage";
+            this.cbImage.Size = new System.Drawing.Size(156, 24);
+            this.cbImage.TabIndex = 1;
+            this.cbImage.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.cbImage.SelectedValueChanged += new System.EventHandler(this.cbImage_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 450);
+            this.Controls.Add(this.cbImage);
+            this.Controls.Add(this.pcbImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbImage;
+        private System.Windows.Forms.ComboBox cbImage;
+    }
+}
+
---
+++ PictureBoxGUI/PictureBoxGUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxGUI
+{
+    public partial class Form1 : Form
+    {
+        string extention = ".jpg";
+        public Form1()
+        {
+            InitializeComponent();
+
+            cbImage.SelectedValueChanged += cbImage_SelectedValueChanged;
+            LoadImageName();
+   
+        }
+        void LoadImageName(){
+            List<string> ListImage = new List<string>(){"1", "2","3"};
+            cbImage.DataSource  = ListImage;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbImage_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            if (cb.SelectedValue != null) {
+                Bitmap bm = new Bitmap(Application.StartupPath + "\\Resources\\" + cb.SelectedValue.ToString() + extention);
+                pcbImage.Image = bm;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
