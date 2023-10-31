6654dd744574f3fbeb22f4b708d0e8b91eeda54b
husthunterpy01 <dangminhhust193231@gmail.com>
2023-10-31 18:35:46 +0700

    API Button

---
+++ ButtonAPI_GUI/ButtonAPI_GUI/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ButtonAPI_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(int h, string m, string c, int type);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           MessageBox(0, "API Message Box", "API Demo", 0);
+           
+        }
+    }
+}
